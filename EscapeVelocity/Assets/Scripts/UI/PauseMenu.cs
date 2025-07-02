168bd6a5359a658c38cbc960f340c61e1f586627
Dylano28 <Dylano.Bakmeijer@stichtingheldeninit.nl>
2025-07-02 11:07:11 +0200

    Current game

--- EscapeVelocity/Assets/Scripts/UI/PauseMenu.cs
+++ EscapeVelocity/Assets/Scripts/UI/PauseMenu.cs
@@ -15,11 +15,10 @@ public class PauseMenu : MonoBehaviour
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.JoystickButton2))
+        if (Input.GetKeyDown(KeyCode.Joystick1Button2))
         {
             if (isPaused)
             {
-                Debug.Log("pause");
                 ResumeGame();
             }
             else
